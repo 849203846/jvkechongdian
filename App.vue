d47e370715091bbdb12edd4876c7419851df9fc1
MC Bennett <849203846@qq.com>
2023-11-28 11:08:53 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+	 globalData: {  
+	            latitude: '' ,
+				 longitude: ''
+	    },
+	onLaunch: function(options) {
+		const {query = {}} = options
+		if(query && getApp()) {
+			getApp().globalData.code = query.name || query.qrCode ? query.qrCode.split('code=')[1] : ''
+		}
+	},
+};
+</script>
+
+<style>
+/* 解决头条小程序组件内引入字体不生效的问题 */
+/* #ifdef MP-TOUTIAO */
+@font-face {
+	font-family: uniicons;
+	src: url('/static/uni.ttf');
+}
+/* #endif */
+</style>
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# jvkechongdian
+# jvke
